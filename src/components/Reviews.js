7f33145e5c0cd8ffe3702f290793baddbc85ce6a
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2023-07-26 12:40:42 -0400

    getloanr fix

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -85,12 +85,12 @@ export default function Reviews() {
                         )
                     })
                 }
-                <button className='back' onClick={()=> setIndex(index - 1)}>
+                <div className='back' onClick={()=> setIndex(index - 1)}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><defs><clipPath><path fill="#00f" fill-opacity=".514" d="m-7 1024.36h34v34h-34z"/></clipPath><clipPath><path fill="#aade87" fill-opacity=".472" d="m-6 1028.36h32v32h-32z"/></clipPath></defs><path d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373" transform="matrix(.03541-.00013.00013.03541 2.98 3.02)" fill="#4d4d4d"/></svg>
-                </button>
-                <button className='forward' onClick={()=> setIndex(index + 1)}>
+                </div>
+                <div className='forward' onClick={()=> setIndex(index + 1)}>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22"><defs><clipPath><path fill="#00f" fill-opacity=".514" d="m-7 1024.36h34v34h-34z"/></clipPath><clipPath><path fill="#aade87" fill-opacity=".472" d="m-6 1028.36h32v32h-32z"/></clipPath></defs><path d="m345.44 248.29l-194.29 194.28c-12.359 12.365-32.397 12.365-44.75 0-12.354-12.354-12.354-32.391 0-44.744l171.91-171.91-171.91-171.9c-12.354-12.359-12.354-32.394 0-44.748 12.354-12.359 32.391-12.359 44.75 0l194.29 194.28c6.177 6.18 9.262 14.271 9.262 22.366 0 8.099-3.091 16.196-9.267 22.373" transform="matrix(.03541-.00013.00013.03541 2.98 3.02)" fill="#4d4d4d"/></svg>
-                    </button>
+                    </div>
                 <div className='circles'>
                     {
                         reviews.map((e, idx) => {
@@ -127,12 +127,11 @@ justify-content: center;
 margin: auto;
 margin-top: 80px;
 position: relative;
-> button {
+> .back, .forward {
     position: absolute;
     background-color: ${props => props.theme.white};
     height: 30px;
     width: 30px;
-    font-size: 17px;
     display: flex;
     cursor: pointer;
     justify-content: center;
@@ -143,8 +142,9 @@ position: relative;
     color: ${props => props.theme.brown};
     border: none;
     svg {
-        height: 15px;
-        width: 15px;
+        height: 20px;
+        width: 20px;
+        z-index: 10;
     }
 }
 .back {
